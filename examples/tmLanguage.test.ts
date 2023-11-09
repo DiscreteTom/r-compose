6f692e99c52eddf9c227ad7897f49f45497180fc
DiscreteTom <discrete_tom@outlook.com>
2023-11-09 20:03:24 +0800

    tests: update example

--- examples/tmLanguage.test.ts
+++ examples/tmLanguage.test.ts
@@ -3,16 +3,14 @@ import { compose } from "../src";
 // ref: https://github.com/microsoft/vscode/blob/cb9c48673f166ae72f55cf19d6f77da518244180/extensions/typescript-basics/syntaxes/TypeScript.tmLanguage.json#L298C19-L298C19
 
 const r = compose(
-  ({ concat, lookahead, lookbehind, group, select, optional, capture }) =>
+  ({ concat, lookahead, lookbehind, select, optional, capture }) =>
     // the whole regex is a lookahead, no characters are consumed
     lookahead(
       concat(
         lookbehind("[_$[:alnum:]]", { negative: true }), // ensure not preceded by an alphanumeric character, underscore or a dollar sign
-        group(
-          select(
-            lookbehind(/\.\.\./), /// could be preceded by 3 dots
-            lookbehind(/\./, { negative: true }), // not preceded by one dot
-          ),
+        select(
+          lookbehind(/\.\.\./), /// could be preceded by 3 dots
+          lookbehind(/\./, { negative: true }), // not preceded by one dot
         ),
         // the `export` keyword is optional, ensure boundary behind, whitespace after
         optional(concat(capture(/\bexport/), /\s+/)),
@@ -23,14 +21,14 @@ const r = compose(
         // ensure not followed by an alphanumeric character, underscore or a dollar sign. e.g. `var_`
         lookahead("[_$[:alnum:]]", { negative: true }),
         // could be followed by 3 dots, but couldn't followed by one dot
-        group(select(lookahead(/\.\.\./), lookahead(/\./, { negative: true }))),
+        select(lookahead(/\.\.\./), lookahead(/\./, { negative: true })),
       ),
     ),
 );
 
 test("TypeScript.tmLanguage.json", () => {
   expect(r.source).toBe(
-    /(?=(?<![_$[:alnum:]])(?:(?<=\.\.\.)|(?<!\.))(?:(\bexport)\s+)?(?:(\bdeclare)\s+)?\b(var|let)(?![_$[:alnum:]])(?:(?=\.\.\.)|(?!\.)))/
+    /(?=(?<![_$[:alnum:]])(?:(?<=\.\.\.)|(?<!\.))(?:(\bexport)\s+)?(?:(\bdeclare)\s+)?\b((?:var|let))(?![_$[:alnum:]])(?:(?=\.\.\.)|(?!\.)))/
       .source,
   );
 });
